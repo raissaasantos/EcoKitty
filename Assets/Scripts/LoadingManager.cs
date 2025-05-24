14027385c2a78a5d8a29890647d2a5079e0e71fc
Raissa <raissaassissantos7@gmail.com>
2025-05-24 20:50:36 -0300

    Build: Level 2

--- Assets/Scripts/LoadingManager.cs
+++ Assets/Scripts/LoadingManager.cs
@@ -5,7 +5,7 @@ public class LoadingManager : MonoBehaviour
 {
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
-            SceneManager.LoadScene(1);
+        //if (Input.GetKeyDown(KeyCode.F))
+        //    SceneManager.LoadScene(1);
     }
 }
